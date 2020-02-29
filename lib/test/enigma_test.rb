a0f3464bbce1e12542726bbd2b488adeddf2427d
Chris Stankus <chrisstankus99@gmail.com>
2020-02-28 21:39:13 -0700

    Create test helper and rakefile. Create basic setup for enigma test.

---
+++ lib/test/enigma_test.rb
@@ -0,0 +1,16 @@
+require_relative 'test_helper'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    
+  end
+
+  def test_it_exists
+    assert_instance_of
+  end
+
+  def test_attributes
+  end
+end
