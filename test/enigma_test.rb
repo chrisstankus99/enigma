ca805c8ee8e0e916527cabe8a6e6f6137325b3a4
Chris Stankus <chrisstankus99@gmail.com>
2020-03-03 01:06:25 -0700

    Remove comments and unnecessary test setup.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -128,11 +128,6 @@ class EnigmaTest < Minitest::Test
     })
     @enigma.stubs(:message).returns("hello world")
     expected = {
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-    }
-    expected = {
       decryption: "hello world",
       key: "02715",
       date: "040895"
@@ -140,23 +135,3 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
   end
 end
-# # decrypt a message with a key and date
-# pry(main) > enigma.decrypt("keder ohulw", "02715", "040895")
-# #=>
-  # {
-  #   decryption: "hello world",
-  #   key: "02715",
-  #   date: "040895"
-  # }
-#
-# # encrypt a message with a key (uses today's date)
-# pry(main)> encrypted = enigma.encrypt("hello world", "02715")
-# #=> # encryption hash here
-#
-# #decrypt a message with a key (uses today's date)
-# pry(main) > enigma.decrypt(encrypted[:encryption], "02715")
-# #=> # decryption hash here
-#
-# # encrypt a message (generates random key and uses today's date)
-# pry(main)> enigma.encrypt("hello world")
-# #=> # encryption hash here
