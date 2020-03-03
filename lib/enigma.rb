2319d2d1ef1b292acc26a5dfd35185fd1a5e4edb
Chris Stankus <chrisstankus99@gmail.com>
2020-03-02 19:55:01 -0700

    Move all methods/classes and their tests into enigma and enigma tests. Create a few stubs in tests.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,9 +1,56 @@
+require 'date'
+
 class Enigma
+
   def initialize
-    @offset = Offset.new
-    @key = Key.new
+  end
+
+  def letters
+    ("a".."z").to_a << " "
+  end
+
+  def generate_keys
+    rand(1..99999).to_s.rjust(5, "0")
+  end
+
+  def generate_date
+    Date.today.strftime("%m%d%y")
+  end
+
+  def last_four_of_date_squared(generate_date)
+    (generate_date.to_i * generate_date.to_i).to_s[-4, 4]
+  end
+
+  def date_sets
+    date_set_hash = {}
+    date_set_hash["A"] = last_four_of_date_squared(generate_date).scan(/\d/).join('')[0, 1].to_i
+    date_set_hash["B"] = last_four_of_date_squared(generate_date).scan(/\d/).join('')[1, 1].to_i
+    date_set_hash["C"] = last_four_of_date_squared(generate_date).scan(/\d/).join('')[1, 2].to_i
+    date_set_hash["D"] = last_four_of_date_squared(generate_date).scan(/\d/).join('')[3, 3].to_i
+    date_set_hash
+  end
+
+  def key_sets
+    key_sets_hash = {}
+    key_sets_hash["A"] = generate_keys.scan(/\d/).join('')[0, 2].to_i
+    key_sets_hash["B"] = generate_keys.scan(/\d/).join('')[1, 2].to_i
+    key_sets_hash["C"] = generate_keys.scan(/\d/).join('')[2, 2].to_i
+    key_sets_hash["D"] = generate_keys.scan(/\d/).join('')[3, 3].to_i
+    key_sets_hash
+  end
+
+  def shift
+    date_sets.merge(key_sets) do |set_letter, old_num, new_num|
+      old_num += new_num
+    end
   end
 
   def encrypt(message, key, date)
+    # shift = Shift.new(date, key)
+    # shift.find_shift.each_value do |shift_position|
+    #   encrypter  = Hash[@char_set.zip(@char_set.rotate(shift_position))]
+    #   message.chars.map { |c| encrypter.fetch(c, " ") }
+    #   require "pry"; binding.pry
+    # end
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,7 +1,6 @@
 require_relative 'test_helper'
+require 'mocha/minitest'
 require './lib/enigma'
-require './lib/offset'
-require './lib/key'
 require 'date'
 
 class EnigmaTest < Minitest::Test
@@ -17,8 +16,71 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_letters
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+       "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @enigma.letters
+  end
+
+  def test_generate_keys
+    assert_instance_of String, @enigma.generate_keys
+    assert_equal 5, @enigma.generate_keys.size
+  end
+
+  def test_generate_date
+    assert_instance_of String, @enigma.generate_date
+    assert_equal 6, @enigma.generate_date.size
+  end
+
+  def test_last_four_of_date_squared
+    assert_instance_of String, @enigma.last_four_of_date_squared(@enigma.generate_date)
+    assert_equal 4, @enigma.last_four_of_date_squared(@enigma.generate_date).size
+  end
+
+  def test_date_sets
+    @enigma.stubs(:generate_date).returns("040895")
+    expected = {
+      "A" => 1,
+      "B" => 0,
+      "C" => 2,
+      "D" => 5
+    }
+    assert_equal expected, @enigma.date_sets
+  end
+
+  def test_key_sets
+    @enigma.stubs(:generate_keys).returns("02715")
+    expected = {
+      "A" => 02,
+      "B" => 27,
+      "C" => 71,
+      "D" => 15
+    }
+    assert_equal expected, @enigma.key_sets
+  end
+
+  def test_enigma_can_shift
+    @enigma.stubs(:date_sets).returns({
+      "A" => 1,
+      "B" => 0,
+      "C" => 2,
+      "D" => 5})
+    @enigma.stubs(:key_sets).returns({
+      "A" => 02,
+      "B" => 27,
+      "C" => 71,
+      "D" => 15})
+    expected = {
+      "A"=>3,
+      "B"=>27,
+      "C"=>73,
+      "D"=>20
+    }
+    assert_equal expected, @enigma.shift
+  end
+
   def test_enigma_can_encrypt
-    # skip
+    skip
     expected = {
         encryption: "keder ohulw",
         key: "02715",
